5b757dbc0e0b7ed299ff79ab3deecd7b9a3abc71
yraviui <yravi.ui@gmail.com>
2026-05-11 19:21:28 +0530

    mailer update

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
